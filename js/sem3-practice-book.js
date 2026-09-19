e6b305d1204124c31f333bedae25821911097eb8
Krish Patel <sp594809@gmail.com>
2026-09-19 22:23:00 +0530

    SEM-3 aggregator fix

--- js/sem3-practice-book.js
+++ js/sem3-practice-book.js
@@ -1,9 +1,10 @@
 // SEM-3 Practice Book aggregator
 const PRACTICE_BOOK_SEM3 = {};
-[1,2,3,4,5].forEach(function(u) {
-  const key = 'SEM3_UNIT_' + u;
-  if (typeof window[key] !== 'undefined') PRACTICE_BOOK_SEM3['unit' + u] = window[key];
-});
+if (typeof SEM3_UNIT_1 !== "undefined") PRACTICE_BOOK_SEM3.unit1 = SEM3_UNIT_1;
+if (typeof SEM3_UNIT_2 !== "undefined") PRACTICE_BOOK_SEM3.unit2 = SEM3_UNIT_2;
+if (typeof SEM3_UNIT_3 !== "undefined") PRACTICE_BOOK_SEM3.unit3 = SEM3_UNIT_3;
+if (typeof SEM3_UNIT_4 !== "undefined") PRACTICE_BOOK_SEM3.unit4 = SEM3_UNIT_4;
+if (typeof SEM3_UNIT_5 !== "undefined") PRACTICE_BOOK_SEM3.unit5 = SEM3_UNIT_5;
 function getAllSem3MCQs() {
   const out = [];
   Object.keys(PRACTICE_BOOK_SEM3).forEach(function (k) {
@@ -11,4 +12,4 @@ function getAllSem3MCQs() {
   });
   return out;
 }
-console.log('✅ SEM-3 Practice Book loaded:', getAllSem3MCQs().length, 'MCQs');
+console.log("✅ SEM-3 Practice Book loaded:", getAllSem3MCQs().length, "MCQs");
